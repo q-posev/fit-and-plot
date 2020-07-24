070de40f23c07c19a2d454a3ff9089987fb3467e
evgeny <evgeny@evgeny-Latitude-7480>
2020-07-24 16:04:35 +0200

    ENH: more pythonic

--- plt-aver-pop-adv.py
+++ plt-aver-pop-adv.py
@@ -1,177 +1,180 @@
-import sys
+from sys import argv,exit
+from re import search as reg_search
+from imp import find_module
 # perform sanity check of required modules
-import imp
 try:
-    imp.find_module('numpy')
-    foundnp = True
+    find_module('numpy')
 except ImportError:
-    foundnp = False
+    print("Numpy is required")
 try:
-    imp.find_module('matplotlib')
-    foundplot = True
+    find_module('matplotlib')
 except ImportError:
-    foundplot = False
+    print("Matplotlib is required")
 try:
-    imp.find_module('pandas')
-    foundpd = True
+    find_module('pandas')
 except ImportError:
-    foundpd = False
+    print("Pandas is required")
 try:
-    imp.find_module('scipy')
-    foundsci = True
+    find_module('scipy')
 except ImportError:
-    foundsci = False
-if not foundnp:
-    print("Numpy is required. Exiting")
-    sys.exit()
-if not foundplot:
-    print("Matplotlib is required. Exiting")
-    sys.exit()
-if not foundpd:
-    print("Pandas is required. Exiting")
-    sys.exit()
-if not foundsci:
-    print("Scipy is required. Exiting")
-    sys.exit()
-import pandas as pd
+    print("Scipy is required")
+
+from pandas import read_csv
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-#print("Number of arguments: ", len(sys.argv))
-print("The arguments are: " , str(sys.argv))
+print("The arguments are: " , str(argv))
 print('')
 plt_err = False
 prt_mol = False
+do_sum = False
 l1 = 0
 hpc = ''
 n_st = 0
 init_st = 0
 #------------------- SEE THE EXECUTION EXAMPLE BELOW: ----------------#
-#--------   python this_script_name.py -h s10 -t 100 -n 9 -s 8 -pe ---#
+#------ python this_script_name.py -h s10 -t 100 -n 9 -s 8 --sum 4 ---#
 #------ Data from: s10; Trajectories: 100; Number of states: 9; ------#
-#-----      Initial state: 8; Plot WITH error bar (-pe option)  ------#
+#-----  Initial state: 8; Sum populations of 4 lowest states    ------#
 #---------------------------------------------------------------------#
 
 # read file todolist with list of jobs to be processed
-todo = pd.read_csv('todolist',skiprows=0,header=None)
+todo = read_csv('todolist',skiprows=0,header=None)
 todo.columns = ["job"]
 
 # read arguments and settings
-for index, arg in enumerate(sys.argv):    
+for index, arg in enumerate(argv):    
     if arg in ['--info', '-i']:
         arg_list=['--hpc', '-h']+['--traj_number', '-t']
         arg_list=arg_list+['--n_states', '-n']+['--init_state', '-s']
         print('Required keywords/arguments: ',arg_list)
-        print('Optional keywords/arguments: [\'--folder\', \'-f\', \'--plt_err\', \'-pe\', \'--mol_name\', \'-m\']')
-        sys.exit()
+        print('Optional keywords/arguments:' +
+                '[\'--sum\', \'--folder\', \'-f\', \'--plt_err\', \'-pe\', \'--mol_name\', \'-m\']')
+        exit()
 
-for index, arg in enumerate(sys.argv):    
+for index, arg in enumerate(argv):    
     if arg in ['--hpc', '-h']:
-        if len(sys.argv) > index + 1:
-          hpc = str(sys.argv[index + 1])
+        if len(argv) > index + 1:
+          hpc = str(argv[index + 1])
           if hpc not in ['s10','oly']:
             print('Available option for HPC: s10, oly')
-            sys.exit()
+            exit()
           else:
-            del sys.argv[index]
-            del sys.argv[index]
+            del argv[index]
+            del argv[index]
             break
         else:
             print('Enter the HPC name: s10 or oly (after --hpc or -h keyword)')
-            sys.exit()
+            exit()
 
 if hpc=='oly':
-    for index, arg in enumerate(sys.argv):
+    for index, arg in enumerate(argv):
         if arg in ['--folder', '-f']:
-            if len(sys.argv) > index + 1:
-                oly_jobid = str(sys.argv[index + 1])
+            if len(argv) > index + 1:
+                oly_jobid = str(argv[index + 1])
                 if oly_jobid=='':
                     print('Folder from OLYMPE has to be the same as slurm job_id')
-                    sys.exit()
+                    exit()
                 else:
-                    del sys.argv[index]
-                    del sys.argv[index]
+                    del argv[index]
+                    del argv[index]
                     break    
             else:
                 print('Enter the name of slurm job_id from OLYMPE (after --folder or -f keyword)')
-                sys.exit()
+                exit()
 
-for index, arg in enumerate(sys.argv):    
+for index, arg in enumerate(argv):    
     if arg in ['--traj_number', '-t']:
-        if len(sys.argv) > index + 1:
-          l1 = int(sys.argv[index + 1])
+        if len(argv) > index + 1:
+          l1 = int(argv[index + 1])
           if l1 < 1:
             print('Number of trajetories has to be > 0')
-            sys.exit()
+            exit()
           else:
-            del sys.argv[index]
-            del sys.argv[index]
+            del argv[index]
+            del argv[index]
             break
         else:
             print('Enter the number of trajectories in TSH (after --traj_number or -t keyword)')
-            sys.exit()
+            exit()
 
-for index, arg in enumerate(sys.argv):
+for index, arg in enumerate(argv):
     if arg in ['--n_states', '-n']:
-        if len(sys.argv) > index + 1:
-          n_st = int(sys.argv[index + 1])
+        if len(argv) > index + 1:
+          n_st = int(argv[index + 1])
           if n_st < 2:
             print('Number of states has to be > 1')
-            sys.exit()
+            exit()
           else:
-            del sys.argv[index]
-            del sys.argv[index]
+            del argv[index]
+            del argv[index]
             break
         else:
             print('Enter the number of states in TSH (after --n_states or -n keyword)')
-            sys.exit()
+            exit()
     
-for index, arg in enumerate(sys.argv):
+for index, arg in enumerate(argv):
     if arg in ['--init_state', '-s']:
-        if len(sys.argv) > index + 1:
-          init_st = int(sys.argv[index + 1])
+        if len(argv) > index + 1:
+          init_st = int(argv[index + 1])
           if init_st < 1:
             print('Initial state has to be > 0')
-            sys.exit()
+            exit()
           else:
-            del sys.argv[index]
-            del sys.argv[index]
+            del argv[index]
+            del argv[index]
             break
         else:
             print('Enter the initial state in TSH (after --init_state or -s keyword)')
-            sys.exit()
+            exit()
 
-for index, arg in enumerate(sys.argv):    
+for index, arg in enumerate(argv):
+    if arg in ['--sum']:
+        if len(argv) > index + 1:
+          pop_to_sum = int(argv[index + 1])
+          do_sum = True
+          if pop_to_sum > n_st-1:
+            print('Too many states to sum')
+            exit()
+          else:
+            del argv[index]
+            del argv[index]
+            break
+        else:
+            print('Enter the number of lower states to be summed (after --sum keyword)')
+            exit()
+
+for index, arg in enumerate(argv):    
     if arg in ['--plt_err', '-pe']:
         plt_err= True
         print('Population will be plotted with error bar')
-        del sys.argv[index]
+        del argv[index]
         break
 
-for index, arg in enumerate(sys.argv):    
+for index, arg in enumerate(argv):    
     if arg in ['--mol_name', '-m']:
-        if len(sys.argv) > index + 1:
-            mol_name = str(sys.argv[index + 1])
+        if len(argv) > index + 1:
+            mol_name = str(argv[index + 1])
             prt_mol = True
-            del sys.argv[index]
-            del sys.argv[index]
+            del argv[index]
+            del argv[index]
             break
         else:
-            print('Enter the molecule name that will go to the the output file name (after --mol_name or -m keyword)')
+            print('Enter the molecule name for the output file (after --mol_name or -m keyword)')
 
 if l1==0 or n_st==0 or init_st==0 or hpc=='':
     print('One of the main arguments is missing, use --info or -i argument for more info')
-    sys.exit()
+    exit()
 
 if hpc=='oly':
     print('Data from OLYMPE machine')
 if hpc=='s10':
     print('Data from s10 machine')
 
-print('Number of trajectories   = '+str(l1))
-print('Number of states in TSH  = '+str(n_st))
-print('Initial state in TSH     = '+str(init_st))
+print('Number of trajectories   = {}'.format(l1))
+print('Number of states in TSH  = {}'.format(n_st))
+print('Initial state in TSH     = {}\n'.format(init_st))
 
 # initialize the population list
 pop_list=[]
@@ -181,83 +184,45 @@ for k in range(1,n_st+1):
 # loop over all trajectories/jobs
 j=0
 for i in range(0,l1):
-
-    # output_name setting
+    # set files with data to be read
     if hpc=='oly':
         pop_file = oly_jobid+str(i) +'.demon' +'/'+'deMon.pop'
     else :
         pop_file = str(todo.job[i])+'/'+'deMon.pop'
-    
     # read populations
-    if i==0 :
-        df0 = pd.read_csv(pop_file,skiprows=0,sep='    ',header=None,engine='python')
+    if i==0:
+        df0 = read_csv(pop_file,skiprows=0,sep='    ',header=None,engine='python')
         df0.columns = ["time"]+pop_list
         l2 = len(df0.time)
-
     # accumulate populations
-    if i>0 :
-        df = pd.read_csv(pop_file,skiprows=0,sep='    ',header=None,engine='python')
+    if i>0:
+        df = read_csv(pop_file,skiprows=0,sep='    ',header=None,engine='python')
         df.columns = ["time"]+pop_list
-
-        df0.pop1 = df0.pop1 + df.pop1
-        df0.pop2 = df0.pop2 + df.pop2
-        df0.pop3 = df0.pop3 + df.pop3
-        df0.pop4 = df0.pop4 + df.pop4
-        df0.pop5 = df0.pop5 + df.pop5
-        df0.pop6 = df0.pop6 + df.pop6
-        df0.pop7 = df0.pop7 + df.pop7
-        df0.pop8 = df0.pop8 + df.pop8
-        #df0.pop9 = df0.pop9 + df.pop9
-        #df0.pop10 = df0.pop10 + df.pop10
-        #df0.pop11 = df0.pop11 + df.pop11
-        #df0.pop12 = df0.pop12 + df.pop12
-        #df0.pop13 = df0.pop13 + df.pop13
-        #df0.pop14 = df0.pop14 + df.pop14
-        #df0.pop15 = df0.pop15 + df.pop15
+        for population in pop_list:
+            df0[population] = df0[population] + df[population]
     
     j+=1
 
+#print("  Number of processed trajectories: {}".format(j))
+# average the populations over ensemble
+for population in pop_list:
+    df0[population] = df0[population]/j
 
-print("Processed jobs: "+str(j))
-
-# compute averaged population
-df0.pop1 = df0.pop1/j
-df0.pop2 = df0.pop2/j
-df0.pop3 = df0.pop3/j
-df0.pop4 = df0.pop4/j
-df0.pop5 = df0.pop5/j
-df0.pop6 = df0.pop6/j
-df0.pop7 = df0.pop7/j
-df0.pop8 = df0.pop8/j
-#df0.pop9 = df0.pop9/j
-#df0.pop10 = df0.pop10/j
-#df0.pop11 = df0.pop11/j
-#df0.pop12 = df0.pop12/j
-#df0.pop13 = df0.pop13/j
-#df0.pop14 = df0.pop14/j
-#df0.pop15 = df0.pop15/j
-
-#---------------------------------------------------------------------#
-#--------- REPLACE df0.popX BELOW WITH df0.popINIT_STATE -------------#
-#---------------------------------------------------------------------#
-A_step = min(df0.pop7)
-#---------------------------------------------------------------------#
 # fitting parameters
+A_step = min(df0['pop'+str(init_st)])
 A_decay = 1./(1.+A_step)
 A_step2 = A_step/(1.+A_step)
-#print(A_step)
 # fitting function
 def exp_func(x, b):
         return (np.exp(-b*x)+A_step)*A_decay
 t=df0.time
 fit_s2 = np.zeros(l2)
 #---------------------------------------------------------------------#
-#--------- REPLACE df0.popX BELOW WITH df0.popINIT_STATE -------------#
+#--------- FIT THE INITIAL STATE POPULATION WITH EXPONENT ------------#
 #---------------------------------------------------------------------#
-# do the fitting
-popt,pcov = curve_fit(exp_func,t,df0.pop7,p0=(0.025))
+popt,pcov = curve_fit(exp_func,t,df0['pop'+str(init_st)],p0=(0.025))
 #---------------------------------------------------------------------#
-print('S'+str(init_st)+' time decay =', (1.0/popt),'fs')
+print('  Decay time of S{0} = {1:.3f} fs \n'.format(init_st,float(1./popt)))
 fit_s2 = exp_func(t,*popt)
 #---------------------------------------------------------------------#
 #---------------- STATISTICAL ERROR ESTIMATION -----------------------#
@@ -265,55 +230,71 @@ fit_s2 = exp_func(t,*popt)
 if plt_err:
     fit_max = np.zeros(l2)
     fit_min = np.zeros(l2)
-
     eps = 0.98/np.sqrt(l1)
     tau_fit = 1.0/popt
-    tau_spec = eps*1.0/popt 
-    print(tau_spec, tau_fit)
-
-    t_max = tau_fit+tau_spec
-    t_min = tau_fit-tau_spec
-    print(t_max,t_min)
-
-    fit_max = exp_func(t,1./t_max)
-    fit_min = exp_func(t,1./t_min)
+    tau_max = (1.+eps)*tau_fit
+    tau_min = (1.-eps)*tau_fit
+    #print(tau_max,tau_min)
+    fit_max = exp_func(t,1./tau_max)
+    fit_min = exp_func(t,1./tau_min)
 #---------------------------------------------------------------------#
-#------------------------- MATPLOTLIB SETTINGS -----------------------#
+#--------------------- MATPLOTLIB SETTINGS ---------------------------#
 #---------------------------------------------------------------------#
 font = {'size'   : 18}
 plt.rc('font', **font)
-fig = plt.figure()
-plt.grid(True)
-ax1 = fig.add_subplot(111)
+ax1 = plt.subplot(111)
+ax1.grid()
+#---------- DO NOT FORGET TO CHANGE THE X- and Y- LIMITS BELOW -------#
 ax1.set_xlim((0.0, 300.0)) 
 ax1.set_ylim((0.0, 1.0))
+#---------------------------------------------------------------------#
 ax1.minorticks_on()
 ax1.tick_params(axis='both',which='minor',length=4,width=1,labelsize=18)
 ax1.tick_params(axis='both',which='major',length=8,width=1,labelsize=18)
 ax1.set_xlabel('Time [fs]')
 ax1.set_ylabel('Population')
 #---------------------------------------------------------------------#
-#------ MODIFY THE PART BELOW TO PLOT THE POPULATIONS OF INTEREST  ---#
+#-----------------  PLOT THE POPULATIONS OF INTEREST  ----------------#
 #---------------------------------------------------------------------#
-ax1.plot(t,df0.pop1+df0.pop2+df0.pop3+df0.pop4,t,df0.pop5,t,df0.pop6,t,df0.pop7,t,df0.pop8,linewidth=2.0)
-#---------------------------------------------------------------------#
+pop_removed = []
+if do_sum:
+    for i in range(2,pop_to_sum+1):
+        pop_add = 'pop'+str(i)
+        df0['pop1'] += df0[pop_add]
+        pop_list.remove(pop_add)
+        pop_removed.append(pop_add)
+
+for population in pop_list:
+    ax1.plot(t,df0[population],linewidth=2.0)
 ax1.plot(t,fit_s2,dashes=[6, 2],color='black',linewidth=2.0) 
 #------------------- PLOT STATISTICAL ERROR --------------------------#
 if plt_err:
     ax1.fill_between(t, fit_min, fit_max, facecolor='lightcoral', alpha=0.5)
 #---------------------------------------------------------------------#
-ax1.legend(('$S_{1-4}$', '$S_{5}$', '$S_{6}$', '$S_{7}$', '$S_{8}$', '$S_{7}$ fit'),loc='upper center', bbox_to_anchor=(0.515, 1.03),ncol=3, fancybox=True, shadow=True) 
+#---------- GENERATE LEGENDS ACCORDING TO THE PLOTTED POPULATIONS ----#
+#---------------------------------------------------------------------#
+legend_list=[]
+if do_sum:
+    legend_sum = '$S_{1-'+str(pop_to_sum)+'}$'
+    legend_list.append(legend_sum)
+else:
+    legend_list.append('$S_{}$'.format('1'))
+for population in pop_list:
+    if population != 'pop1':
+        legend_list.append('$S_{}$'.format(int(reg_search(r'\d+', population)[0])))
+legend_list.append('$S_{}$ fit'.format(init_st))
+
+ax1.legend(legend_list,loc='upper center', bbox_to_anchor=(0.515, 1.28), ncol=3, fancybox=True, shadow=True) 
 #------------------------- SET OUTPUT FILENAME -----------------------#
-fileformat = '.png'
 if prt_mol:
     output_name = '{}-'.format(mol_name)
 else:
     output_name = ''
-output_name = output_name + 'pop-{0}traj-initST-{1}-totalST-{2}'.format(l1,init_st,n_st)
-if plt_err:
-    output_name = output_name + '-wERR'
+output_name = output_name + 'pop-{0}traj-initST-{1}-totalST-{2}-tau-{3:.0f}.'.format(l1,init_st,n_st,float(1./popt))
+#------------------------- SET OUTPUT FORMAT -------------------------#
+fileformat = 'png'
 output_name = output_name + fileformat
-
+#------------------------- SAVE THE FIGURE  --------------------------#
 print('Output file: {}'.format(output_name))
-plt.savefig(output_name, bbox_inches='tight',format=fileformat[1:4],dpi=600)
+plt.savefig(output_name, bbox_inches='tight',format=fileformat,dpi=600)
 
